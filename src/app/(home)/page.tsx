d9989d6b67422ba3813fb1258b364c1b8ecf3eaf
rafaelluiis2315 <rafael.barreto@liferay.com>
2023-12-15 23:55:37 -0300

    feat: add first descount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        alt="Até 55% de desconto só esse mês"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+    </div>
+  );
+}
