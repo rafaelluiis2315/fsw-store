c0b7b2eb0c22251d68f1469f611a9c71fa35fc7d
rafaelluiis2315 <rafael.barreto@liferay.com>
2024-01-04 16:50:36 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -12,25 +12,34 @@ export default async function Home() {
     },
   });
 
-
   return (
     <div className="">
       <Image
         src="/banner-home-01.png"
-        alt="Até 55% de desconto só esse mês"
+        alt="Até 55% de desconto só esse mês!"
         height={0}
         width={0}
         className="h-auto w-full px-5 pt-8"
         sizes="100vw"
       />
+
       <div className="mt-8 px-5">
         <Categories />
       </div>
 
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
 
+      <Image
+        src="/banner-home-02.png"
+        alt="Até 55% de desconto em mouses!"
+        height={0}
+        width={0}
+        className="h-auto w-full px-5 pt-8"
+        sizes="100vw"
+      />
     </div>
   );
 }
