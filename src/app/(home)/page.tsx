1a26ac970d590ed5fc8038323a5ce2f002b131b4
rafaelluiis2315 <rafael.barreto@liferay.com>
2024-01-04 16:30:33 -0300

    feat: add ProductItem and ProductList components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,36 @@
 import Image from "next/image";
 import Categories from "./components/categories";
+import { prismaClient } from "@/lib/prisma";
+import ProductList from "./components/product-list";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
+
   return (
-    <div className="p-5">
+    <div className="">
       <Image
         src="/banner-home-01.png"
         alt="Até 55% de desconto só esse mês"
         height={0}
         width={0}
-        className="h-auto w-full"
+        className="h-auto w-full px-5 pt-8"
         sizes="100vw"
       />
-      <div className="mt-8">
+      <div className="mt-8 px-5">
         <Categories />
       </div>
+
+      <div className="mt-8">
+        <ProductList products={deals} />
+      </div>
+
     </div>
   );
 }
