dc4f13ee3b9461492fb977c9db3b41852eddcfdf
rafaelluiis2315 <rafael.barreto@liferay.com>
2024-01-18 20:04:52 -0300

    style: change cart width

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -125,7 +125,7 @@ const Header = () => {
           </Button>
         </SheetTrigger>
 
-        <SheetContent>
+        <SheetContent className="w-[350px]">
           <Cart />
         </SheetContent>
       </Sheet>
