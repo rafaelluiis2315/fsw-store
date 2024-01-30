fd8ce3ce8186c037360bda41e0f83c61440d2c9b
rafaelluiis2315 <rafael.barreto@liferay.com>
2024-01-30 18:47:30 -0300

    feat: ensure product details page in responsive on desktop

--- src/app/product/[slug]/components/product-info.tsx
+++ src/app/product/[slug]/components/product-info.tsx
@@ -29,20 +29,22 @@ const ProductInfo = ({ product }: ProductInfoProps) => {
   };
 
   return (
-    <div className="flex flex-col px-5">
-      <h2 className="text-lg">{product.name}</h2>
+    <div className="flex flex-col px-5 lg:w-[40%] lg:rounded-lg lg:bg-accent lg:p-10">
+      <h2 className="text-lg lg:text-2xl">{product.name}</h2>
       <div className="intens-center flex gap-1">
-        <h1 className="text-xl font-bold">
+        <h1 className="text-xl font-bold lg:text-3xl">
           R$ {product.totalPrice.toFixed(2)}
         </h1>
         {product.discountPercentage > 0 && (
-          <DiscountBedge>{product.discountPercentage}</DiscountBedge>
+          <DiscountBedge className="lg:text-base">
+            {product.discountPercentage}
+          </DiscountBedge>
         )}
       </div>
 
       {product.discountPercentage > 0 && (
-        <p className="text-sm opacity-75">
-          De:{" "}
+        <p className="text-sm opacity-75 lg:text-base">
+          De:
           <span className="line-through">
             R$ {Number(product.basePrice).toFixed(2)}
           </span>
@@ -73,7 +75,7 @@ const ProductInfo = ({ product }: ProductInfoProps) => {
         Adicionar ao Carrinho
       </Button>
 
-      <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2">
+      <div className="mt-5 flex items-center justify-between rounded-lg bg-accent px-5 py-2 lg:bg-[#2A2A2A]">
         <div className="flex items-center gap-2">
           <TruckIcon />
           <div className="fle flex-col">
