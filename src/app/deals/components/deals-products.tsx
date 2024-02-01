9fc079a7326e6f78523b1cc80b16b805caa31ba7
rafaelluiis2315 <rafael.barreto@liferay.com>
2024-02-01 09:10:28 -0300

    feat: add ProductSkeletonList on deals page

---
+++ src/app/deals/components/deals-products.tsx
@@ -0,0 +1,26 @@
+import ProducItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { prismaClient } from "@/lib/prisma";
+
+const DealsProducts = async () => {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+  
+  return (
+    <>
+      {deals.map((product) => (
+        <ProducItem
+          key={product.id}
+          product={computeProductTotalPrice(product)}
+        />
+      ))}
+    </>
+  );
+};
+
+export default DealsProducts;
