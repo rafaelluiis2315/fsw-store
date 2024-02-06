8b877a64081abb25638e03db1b5bd87d11180045
rafaelluiis2315 <rafael.barreto@liferay.com>
2024-02-06 12:12:48 -0300

    feat: add FormLogin component

---
+++ src/app/auth/login/components/form-login.tsx
@@ -0,0 +1,80 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+
+const registerSchema = z.object({
+  email: z.string().email({ message: "Invalid email." }),
+  password: z.string(),
+});
+
+const FormLogin = () => {
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof registerSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+        <div className="flex flex-col gap-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="mt-5 flex justify-center">
+          <Button type="submit" className="w-full md:w-[50%]">
+            Entrar
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default FormLogin;
