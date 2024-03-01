1da2cd4b041085e954c46cda4704fdab04db1838
rafaelluiis2315 <rafael.barreto@liferay.com>
2024-03-01 17:45:55 -0300

    fix: ensure that url of the AWS-S3 object is create with successful

--- src/actions/aws-s3.ts
+++ src/actions/aws-s3.ts
@@ -2,11 +2,16 @@
 
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const bucketName = process.env.AWS_BUCKET_NAME;
+
 const s3Client = new S3Client({
-    region: process.env.AWS_REGION,
+    region: region,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: accessKeyId,
+        secretAccessKey: secretAccessKey,
     },
 });
 
@@ -31,18 +36,18 @@ const uploadImageToS3 = async (file: File): Promise<string> => {
         const fileContent = await file.arrayBuffer();
 
         const body = new Uint8Array(fileContent);
-        const bucketName = process.env.AWS_BUCKET_NAME;
         const key = `${Date.now()}_${file.name}`;
 
         const command = new PutObjectCommand({
             Bucket: bucketName,
             Key: key,
             Body: body,
+            ACL: 'public-read',
         });
 
         const response = await s3Client.send(command);
 
-        const objectUrl = `https://${bucketName}.s3.${s3Client.config.region}.amazonaws.com/${key}`;
+        const objectUrl = `https://${bucketName}.s3.${region}.amazonaws.com/${key}`;
 
         if (response.$metadata.httpStatusCode === 200) {
             return objectUrl;
